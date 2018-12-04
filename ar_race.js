813fcd722441fde9d55e8a509868d16e7d2d9476
andreios <45299090+andreios@users.noreply.github.com>
2018-12-04 17:19:53 -0600

    Java programming
    Racers now successfully move across screen and stop when threshold is reached.  (Threshold may need tweaking.)
    Also adjusted racers to be same width.
    
    Need to implement win screen and reset trigger.

--- ar_race.js
+++ ar_race.js
@@ -7,39 +7,70 @@
    Filename: ar_race.js
 */
 
-//add listener to gate image
+//create global references
+var orc = document.getElementById("hordeRacer");
+var human = document.getElementById("allyRacer");
 var gate = document.getElementById("startgate");
-gate.onclick = runRace;
 
-function runRace() {
-	//swap red gate to green gate
-	gate.src = "gate_green.png";
+var loopClock;
+
+
+
+window.onload = init;
+
+function init(){
+	//initialize racer positions
+	orc.style.left = "0px";
+	human.style.left = "0px";
 	
-	var hordeWin = false;
-	var allyWin = false;
-	
-	advanceRacers();
-	setInterval(advanceRacers,100);
+	//add listener to gate image
+	gate.src = "gate_red.png";
+	gate.onclick = startRace;
 }
 
-function advanceRacers(){
-	//create references for racer images
-	var orc = document.getElementById("hordeRacer");
-	var human = document.getElementById("allyRacer");
+function startRace() {
+	//swap red gate to green gate
+	gate.src = "gate_green.png";
+	gate.onclick = null;
+
+//	runRace();
+	loopClock = setInterval(runRace, 50);
+}
+
+function runRace(){
+	//boolean variables to test win condition; may not be needed
+	var winner = "";
+
+	//establishes target for victory
+	var threshold = (.8 * window.innerWidth)-200;
+
+	advanceRacers(orc);
+	advanceRacers(human);
+
+	if (orc.offsetLeft >= threshold || human.offsetLeft >= threshold) {
+		if(orc.offsetLeft > human.offsetLeft) {
+			winScreen("horde");
+		} else { winScreen("alliance"); }
+	}	
+}
+
+function advanceRacers(racer){
 	
 	//store current image position
-	var orcPos = parseInt(orc.style.right);
-	var humanPos = parseInt(human.style.right);
+	var racerPos = parseInt(racer.style.left);
 	
 	//generate random shift amount
-	var orcShift = Math.floor(5 * Math.random());
-	var humanShift = Math.floor(5 * Math.random());
+	var racerShift = Math.floor(20 * Math.random());
 	
 	//apply shift
-	orcPos += orcShift;
-	humanPos += humanShift;
+	racerPos += racerShift;
 	
-	orc.style.right = (orcPos + "px");
-	human.style.right = (humanPos + "px");
+	racer.style.left = (racerPos + "px");
+}
+
+function winScreen(winner) {		
+	clearInterval(loopClock);
+
+	init();
 }
 
